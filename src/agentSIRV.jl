bf55bc3ba8345ed8abcb4ebf353d977a7b7f1b40
Bartosz Zbik <bartosz@zbik.org>
2022-08-01 00:09:29 +0200

    apply axelrod frames

--- src/agentSIRV.jl
+++ src/agentSIRV.jl
@@ -2,11 +2,13 @@ module agentSIRV
 
 export EpidState, Disease, BasicPopulation, LinkedPopulation,
        get_stats, inital_infecions!,
-       simulate_infections!, time_passes!, vaccinate!
+       simulate_infections!, time_passes!, vaccinate!,
+       apply_frame!
 
 include("basic_structures.jl")
 include("BasicPopulation.jl")
 include("epidemic.jl")
 include("LinkedPopulation.jl")
+include("axelrod_frames.jl")
 
 end
---
+++ src/axelrod_frames.jl
@@ -0,0 +1,21 @@
+
+using DelimitedFiles: readdlm
+
+const frame_dir = (@__DIR__) * "/../axelrod_frames"
+
+function apply_frame!(p::BasicPopulation, frame_number::Integer)::Nothing
+	frame_path = frame_dir * "/$(p.pop_size[1])x$(p.pop_size[2])/$(frame_number).txt"
+	F = readdlm(frame_path, '\t', Int, '\n')
+	apply_frame!(p, F)
+	return nothing
+end
+
+function apply_frame!(p::BasicPopulation, frame::Matrix{Int})::Nothing
+	size(frame) == p.pop_size || error("frame size not compatybile with population size")
+	new_val = rand(maximum(frame))
+	map!(x -> new_val[x], p.vaccination_likelihoods, frame)
+	return nothing
+end
+
+apply_frame!(p::LinkedPopulation, other...) = apply_frame!(p.p, other...)
+
