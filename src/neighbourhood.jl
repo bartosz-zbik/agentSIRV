162c0c9202baca8652bb2ceaff23d5c4bffbaa55
Bartosz Zbik <bartosz@zbik.org>
2022-08-24 11:05:47 +0200

    remove LinkedPopulation form whole project

--- src/neighbourhood.jl
+++ src/neighbourhood.jl
@@ -13,3 +13,14 @@ function generate_neighbourhood(n::Integer)::Matrix{Int}
 	return tmp_nb
 end
 
+function generate_links(dim1::Integer, dim2::Integer, n::Integer)::Matrix{Int}
+	links = Array{Int}(undef, n, 4)
+	for i in 1:n
+		links[i, 1] = rand(1:dim1)
+		links[i, 2] = rand(1:dim2)
+		links[i, 3] = rand(1:dim1)
+		links[i, 4] = rand(1:dim2)
+	end
+	return links
+end
+
