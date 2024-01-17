47456cf28276d8e4c33b3f842dc8d6f8a5de8ac6
Bartosz Zbik <bartosz@zbik.org>
2024-01-17 12:55:15 +0100

    expose Bernoulli vaccination likelihood distribution

--- src/agentSIRV.jl
+++ src/agentSIRV.jl
@@ -4,7 +4,7 @@ export EpidState, Disease, BasicPopulation,
        get_stats, inital_infecions!,
        get_S, get_I, get_R, get_V,
        simulate_infections!, time_passes!, vaccinate!, simulate_step!,
-       apply_frame!
+       apply_frame!, apply_binary_distribution!
 
 include("basic_structures.jl")
 include("BasicPopulation.jl")
